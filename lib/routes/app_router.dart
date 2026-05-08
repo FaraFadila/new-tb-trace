3f8e38eb53bd8819e2d9e85efe2b68fb155dc1bf
FaraFadila <farafadilaputrieffendi@gmail.com>
2026-05-08 15:17:36 +0700

    feat: connect add patient routing

--- lib/routes/app_router.dart
+++ lib/routes/app_router.dart
@@ -14,6 +14,7 @@ import '../features/profile/edit_profile_page.dart';
 
 // ================= PATIENT =================
 import '../features/patient/patient_management_page.dart';
+import '../features/patient/add_patient_page.dart';
 
 final GoRouter appRouter = GoRouter(
   initialLocation: '/',
@@ -52,6 +53,13 @@ final GoRouter appRouter = GoRouter(
     path: '/patient-management',
     builder: (context, state) =>
         const PatientManagementPage(),
+  ),
+
+    // ================= ADD PATIENT =================
+    GoRoute(
+    path: '/add-patient',
+    builder: (context, state) =>
+        const AddPatientPage(),
   ),
 
     // ================= PROFILE =================
